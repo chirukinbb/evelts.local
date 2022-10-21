b77c78b48d9c94e640858ef81d717ec1f427a55c
chirukinbb <chirukinbb@gmail.com>
2022-10-21 12:09:29 +0300

    event + category

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,10 +13,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('v1/auth',[\App\Http\Controllers\Api\UserController::class,'auth']);
+Route::post('v1/auth', [\App\Http\Controllers\Api\UserController::class, 'auth']);
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::post('v1/user/updateData',[\App\Http\Controllers\Api\UserController::class,'update']);
-    Route::post('v1/user/updatePassword',[\App\Http\Controllers\Api\UserController::class,'updatePassword']);
-    Route::post('v1/user/updateEmail',[\App\Http\Controllers\Api\UserController::class,'updateEmail']);
+    Route::post('v1/user/updateData', [\App\Http\Controllers\Api\UserController::class, 'update']);
+    Route::post('v1/user/updatePassword', [\App\Http\Controllers\Api\UserController::class, 'updatePassword']);
+    Route::post('v1/user/updateEmail', [\App\Http\Controllers\Api\UserController::class, 'updateEmail']);
+
+    Route::get('v1/categories', [\App\Http\Controllers\Api\CategoryController::class, 'index']);
+
+    Route::get('v1/events', [\App\Http\Controllers\Api\EventController::class, 'index']);
+    Route::get('v1/event/create', [\App\Http\Controllers\Api\EventController::class, 'create']);
+    Route::get('v1/event/update', [\App\Http\Controllers\Api\EventController::class, 'update']);
+    Route::get('v1/event/{id}', [\App\Http\Controllers\Api\EventController::class, 'get']);
 });
